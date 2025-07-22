cc836749d69e8326c87afdf6a92dd0e26b28ad5d
martin-gabriel42 <gabmar@post.cz>
2025-07-23 00:26:41 +0200

    Added csv_dumper function that writes the results into a file

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ email: gabmar@post.cz
 import sys
 import validators
 import requests as rq
+import csv
 from bs4 import BeautifulSoup as bs
 from pprint import pprint
-
-url_example = "https://www.volby.cz/pls/ps2017/ps3?xjazyk=CZ"
 
 
 def get_content(url):
@@ -68,13 +67,73 @@ def input_validation():
     return output_file_name
 
 
-def find_district_links(parsed_content):
+def get_district_links(parsed_content):
     links = []
     for a_tag in parsed_content.find_all("a", href=True):
         if a_tag['href'][:5] == 'ps32?':
             links.append("https://www.volby.cz/pls/ps2017nss/" + a_tag['href'])
 
     return links
+
+
+def get_district_data(url):
+
+    parsed_html = get_html(url)
+
+    all_rows = [row for row in parsed_html.find_all("tr") if not row.find_all("th")]
+
+    district_data = []
+    
+    for row in all_rows:
+
+        district_info = {}
+        district_info["link"] = "https://www.volby.cz/pls/ps2017nss/" + row.find("a")["href"]
+        district_info["code"] = row.find_all("td")[0].get_text()
+        district_info["municipality"] = row.find_all("td")[1].get_text()
+
+        district_data.append(district_info)
+
+    return district_data
+
+
+def scrape_district(url):
+
+    district_data = get_district_data(url)
+
+    for district in district_data:
+        municipality_data = get_municipality_data(district["link"])
+        print("scaped municipality: ", district["municipality"])
+        for key, value in municipality_data.items():
+            district[key] = value
+
+    return district_data
+
+
+def get_municipality_data(url):
+
+    parsed_html = get_html(url)
+
+    all_tables = parsed_html.find_all("table")
+
+    overview_table_data_row = all_tables[0].find_all("tr")[2].find_all("td")
+    voter_data = {
+        "registered voters": overview_table_data_row[3].get_text(),
+        "total votes": overview_table_data_row[6].get_text(),
+        "valid votes": overview_table_data_row[7].get_text(),
+        "votes by party": None
+    }
+    
+    party_vote_data = []
+    for table in all_tables[1:]:
+        all_rows = [row for row in table.find_all("tr") if not (row.find_all("th") or row.find_all(class_="hidden_td"))]
+        for row in all_rows:
+            all_cells = row.find_all("td")
+            party_vote_data.append((all_cells[1].get_text(), all_cells[2].get_text()))
+
+    voter_data["votes by party"] = party_vote_data
+    #pprint(voter_data)
+
+    return voter_data
 
 
 def get_all_table_rows(parsed_content):
@@ -88,22 +147,54 @@ def get_all_table_rows(parsed_content):
     return all_rows
 
 
+def csv_dumper(district_data, output_file_name):
+
+    parties = list({party_data[0] for municipality_data in district_data for party_data in municipality_data["votes by party"]})
+
+    headers = ["code", "municipality", "registered voters", "total votes", "valid votes"]
+    for party in parties:
+        headers.append(party)
+
+    with open(output_file_name, "w+", newline='', encoding="utf-8") as f:
+       
+        writer = csv.DictWriter(f, fieldnames=headers, restval="0")
+        writer.writeheader()
+       
+        for municipality_data in district_data:
+            row_dict = dict()
+
+            for key, value in municipality_data.items():
+                if key not in ("link", "votes by party"):
+                    row_dict[key] = value
+
+            for key, value in municipality_data["votes by party"]:
+                row_dict[key] = value
+
+            writer.writerow(row_dict)
+
+
+        
+
+
 def main():
+
+    url_example = "https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2101"
     
     output_file_name = input_validation()
     if output_file_name is None:
         sys.exit()
     url = sys.argv[1]
 
-    parsed_html = get_html(url)
 
-    district_links = find_district_links(parsed_html)
+    #pprint(get_district_data("https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2101"))
+    #pprint(get_municipality_data("https://www.volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=2&xobec=529303&xvyber=2101"))
 
-    #todo additional validation
-    
+    district_data = scrape_district(url_example)
 
+    #pprint(district_data)
 
-    pprint(district_links)
+    csv_dumper(district_data, output_file_name)
+
 
 
 if __name__ == "__main__":
